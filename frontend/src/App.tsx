a72ed5c22503ce1f2a524de7735baf12ada708d6
Adedayo <adedayo.dc.sanni@gmail.com>
2026-02-18 21:16:51 -0300

    fix: removendo dependencias desnecessárias

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, useSearchParams } from 'react-router-dom'
+import { Routes, Route, Navigate} from 'react-router-dom'
 
 import AppShell from './components/layout/AppShell/AppShell'
 import Header from './components/layout/Header/Header'
