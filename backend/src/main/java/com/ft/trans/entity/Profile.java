a8e3b3fb1904968632db30d7944048065b5a118f
Marcelo Dias Machado <madias-m@c1r6p1.42sp.org.br>
2026-03-07 13:10:23 -0300

    Criando relacioamento entre User e Profile

--- backend/src/main/java/com/ft/trans/entity/Profile.java
+++ backend/src/main/java/com/ft/trans/entity/Profile.java
@@ -2,16 +2,42 @@ package com.ft.trans.entity;
 
 import java.sql.Date;
 
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.query.common.FetchClauseType;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "profiles")
+@Table(name = "profiles", uniqueConstraints = {
+	@UniqueConstraint(columnNames = {"user_id", "position"})
+})
 public class Profile
 {
+	public enum ProfileType
+	{
+		MENTOR,
+		MENTORADO
+	};
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long		id;
+	@ManyToAny(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
 	public User		user;
 	public String	avatarUrl;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	public String	position;
 	public String	bio;
 	public String	role;
