30e80a7347a636253fb60435d482c92b4f24652d
Fabio <fabiolljunior23@gmail.com>
2026-03-09 20:27:57 -0300

    test(level mock): added to test the storage in database

---
+++ backend/src/main/java/com/ft/trans/configuration/LevelMockConfig.java
@@ -0,0 +1,52 @@
+package com.ft.trans.configuration;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ft.trans.entity.Level;
+import com.ft.trans.repository.LevelRepository;
+
+@Configuration
+public class LevelMockConfig
+{
+
+    @Bean
+    CommandLineRunner loadLevels(LevelRepository levelRepository)
+    {
+        return args ->
+        {
+
+            if (levelRepository.count() > 0)
+                return;
+
+            Level l1 = new Level();
+            l1.level = 1;
+            l1.xp_required = 0;
+
+            Level l2 = new Level();
+            l2.level = 2;
+            l2.xp_required = 100;
+
+            Level l3 = new Level();
+            l3.level = 3;
+            l3.xp_required = 300;
+
+            Level l4 = new Level();
+            l4.level = 4;
+            l4.xp_required = 600;
+
+            Level l5 = new Level();
+            l5.level = 5;
+            l5.xp_required = 1000;
+
+            levelRepository.save(l1);
+            levelRepository.save(l2);
+            levelRepository.save(l3);
+            levelRepository.save(l4);
+            levelRepository.save(l5);
+
+            System.out.println("Levels mock loaded");
+        };
+    }
+}
