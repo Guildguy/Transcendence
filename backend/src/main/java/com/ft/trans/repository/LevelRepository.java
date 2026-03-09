c29de6d537b701efbd94358cc617157e5de37e51
Fabio <fabiolljunior23@gmail.com>
2026-03-09 18:52:26 -0300

    feat(level entity): Add Level entity and repository for gamification

---
+++ backend/src/main/java/com/ft/trans/repository/LevelRepository.java
@@ -0,0 +1,9 @@
+package com.ft.trans.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ft.trans.entity.Level;
+
+public interface LevelRepository extends JpaRepository<Level, Long>
+{
+    Level findByLevel(Integer level);
+}
