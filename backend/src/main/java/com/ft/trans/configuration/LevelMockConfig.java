eec0c881508563786ca26299c7376aa23fece8f9
Fabio <fabiolljunior23@gmail.com>
2026-03-10 16:37:39 -0300

    fix(level mock): update experience points required for levels 2-5

--- backend/src/main/java/com/ft/trans/configuration/LevelMockConfig.java
+++ backend/src/main/java/com/ft/trans/configuration/LevelMockConfig.java
@@ -26,19 +26,19 @@ public class LevelMockConfig
 
             Level l2 = new Level();
             l2.level = 2;
-            l2.xp_required = 100;
+            l2.xp_required = 500;
 
             Level l3 = new Level();
             l3.level = 3;
-            l3.xp_required = 300;
+            l3.xp_required = 1500;
 
             Level l4 = new Level();
             l4.level = 4;
-            l4.xp_required = 600;
+            l4.xp_required = 3000;
 
             Level l5 = new Level();
             l5.level = 5;
-            l5.xp_required = 1000;
+            l5.xp_required = 5000;
 
             levelRepository.save(l1);
             levelRepository.save(l2);
