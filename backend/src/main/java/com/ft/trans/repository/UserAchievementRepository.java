f723ec93ffa97241b0578c07f9f75231cd84e675
Fabio <fabiolljunior23@gmail.com>
2026-03-15 20:59:30 -0300

    feat(user achievement): Add method to find UserAchievements by userId

--- backend/src/main/java/com/ft/trans/repository/UserAchievementRepository.java
+++ backend/src/main/java/com/ft/trans/repository/UserAchievementRepository.java
@@ -1,9 +1,12 @@
 package com.ft.trans.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ft.trans.entity.UserAchievement;
 
 public interface UserAchievementRepository extends JpaRepository<UserAchievement, Long>
 {
     boolean existsByUserIdAndAchievementId(Long userId, Long achievementId);
+    List<UserAchievement> findByUserId(Long userId);
 }
