6a7bde6987d3aaf15653867236d9554ded256602
Fabio <fabiolljunior23@gmail.com>
2026-03-15 21:31:52 -0300

    feat: Add order annotations to CommandLineRunner beans in mock configuration classes

--- backend/src/main/java/com/ft/trans/configuration/UserAchievementMockConfig.java
+++ backend/src/main/java/com/ft/trans/configuration/UserAchievementMockConfig.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 import com.ft.trans.entity.User;
 import com.ft.trans.entity.Achievement;
@@ -19,6 +20,7 @@ public class UserAchievementMockConfig
 {
 
     @Bean
+    @Order(4)
     CommandLineRunner loadUserAchievements(UserRepository userRepo, AchievementRepository achievementRepo, UserAchievementRepository repo)
     {
         return args ->
