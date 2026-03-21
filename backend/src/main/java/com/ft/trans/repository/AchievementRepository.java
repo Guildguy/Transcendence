0e880b56766d58aebacae8e9f60d9dbbfff7642b
Fabio <fabiolljunior23@gmail.com>
2026-03-21 19:40:42 -0300

    feat(achievement repository): Add methods to find achievements by type and name

--- backend/src/main/java/com/ft/trans/repository/AchievementRepository.java
+++ backend/src/main/java/com/ft/trans/repository/AchievementRepository.java
@@ -1,6 +1,12 @@
 package com.ft.trans.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ft.trans.entity.Achievement;
 
-public interface AchievementRepository extends JpaRepository<Achievement, Long> {}
+public interface AchievementRepository extends JpaRepository<Achievement, Long>
+{
+	List<Achievement> findByType(String type);
+	Achievement findByName(String name);
+}
