8ed82e72a673567db803e43b0f363fd43d0884c7
Leticia Sampietro <lsampiet@c3r6p5.42sp.org.br>
2026-03-21 18:25:07 -0300

    separated mock from main component logic

--- frontend/src/pages/logged/HomeLogged.tsx
+++ frontend/src/pages/logged/HomeLogged.tsx
@@ -7,44 +7,9 @@ import UserHeader from '../../components/layout/UserHeader/UserHeader'
 import Avatar from '../../components/common/Avatar/Avatar'
 import Button from '../../components/common/Button/Button'
 import { Check, X } from "lucide-react";
+import { mockRequests, mockSchedule, mockAchievements } from './HomeLogged.mock.tsx'
 import './HomeLogged.css'
 
-interface PendingRequest {
-  id: number
-  name: string
-  avatar?: string
-}
-
-interface ScheduleItem {
-  id: number
-  time: string
-  mentee: string
-}
-
-interface Achievement {
-  id: number
-  title?: string
-  icon?: string
-}
-
-const mockRequests: PendingRequest[] = [
-  { id: 1, name: 'Vítoria' },
-  { id: 2, name: 'Vítoria' },
-  { id: 3, name: 'Carlos' },
-]
-
-const mockSchedule: ScheduleItem[] = [
-  { id: 1, time: '08:00h', mentee: 'Mentoria Carla' },
-  { id: 2, time: '10:00h', mentee: 'Mentoria Carla' },
-  { id: 3, time: '15:00h', mentee: 'Mentoria Carla' },
-  { id: 4, time: '19:00h', mentee: 'Mentoria Carla' },
-  { id: 5, time: '20:30h', mentee: 'Mentoria Carla' },
-]
-
-const mockAchievements: Achievement[] = [
-  { id: 1 }, { id: 2 }, { id: 3 },
-  { id: 4 }, { id: 5 }, { id: 6 },
-]
 
 const handleAccept = (id: number) => {
   console.log(`Accepted request with ID: ${id}`);
@@ -60,6 +25,7 @@ function HomeLogged() {
 
   return (
     <AppShell
+      sidebar={null}
       header={<Header isAuthenticated={true} />}
       footer={<Footer />}
     >
