fdb57d3917841c2772495151179bfc15d7485d47
Adedayo Sanni <asanni@c1r2p2.42sp.org.br>
2026-03-22 17:21:26 -0300

    Adicionando anosExperiencia e criando endpoint para salvar imagem

--- backend/src/main/java/com/ft/trans/controller/ProfileController.java
+++ backend/src/main/java/com/ft/trans/controller/ProfileController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ft.trans.dto.ProfileImageDTO;
 import com.ft.trans.dto.UpdateProfileDTO;
 import com.ft.trans.entity.Profile;
 import com.ft.trans.service.ProfileService;
@@ -43,6 +45,21 @@ public class ProfileController {
 				.body(result.validationResult().getErrors());
 		}
 		return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(result.entity());
+    }
+
+    @PostMapping("/image")
+    public ResponseEntity<?> uploadProfileImage(@RequestBody ProfileImageDTO imageDTO) {
+        Result result = this.profileService.saveProfileImage(imageDTO);
+
+        if (result.validationResult().hasErrors())
+        {
+            return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_CONTENT)
+                .body(result.validationResult().getErrors());
+        }
+        return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(result.entity());
     }
