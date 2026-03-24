a9e563d0db1484c8699cf7b1acbcad3814d25224
mmachadociandt <mmachado@ciandt.com>
2026-03-23 21:26:15 -0300

    Ajuste na autenticacao

--- backend/src/main/java/com/ft/trans/configuration/WebConfig.java
+++ backend/src/main/java/com/ft/trans/configuration/WebConfig.java
@@ -1,30 +1,20 @@
 package com.ft.trans.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import com.ft.trans.filter.JwtAuthenticationFilter;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
     
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
-    
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "http://0.0.0.0:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type");
-    }
 
     /**
      * Registra o filtro de autenticação JWT
-     * Será aplicado a todas as requisições
+     * Será aplicado a todas as requisições exceto /login e /users (POST)
      */
     @Bean
     public FilterRegistrationBean<JwtAuthenticationFilter> jwtFilter() {
