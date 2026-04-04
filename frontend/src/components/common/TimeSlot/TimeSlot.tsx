0a383dc991b9835d7d10b099874cb149cd3a9d93
Leticia S <lesampietro@gmail.com>
2026-04-04 17:47:06 -0300

    :boom: fix: time slots are filtered against duplicated or oerlaping values

--- frontend/src/components/common/TimeSlot/TimeSlot.tsx
+++ frontend/src/components/common/TimeSlot/TimeSlot.tsx
@@ -122,10 +122,6 @@ const AddTimeSlotModal: React.FC<AddTimeSlotModalProps> = ({ day, onClose, onCon
     const endTime = `${endHour}:${endMinute}`;
 
     onConfirm(startTime, endTime);
-    toast({ 
-      title: 'Nova disponibilidade adicionada a agenda.',
-      description: `${startTime} - ${endTime}`
-    });
   };
 
   const handleStartMinuteChange = (value: string) => {
